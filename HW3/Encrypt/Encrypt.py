3ef10c15528c08d26e2853b500491e990e72faee
jacky861132 <jj861119@gmail.com>
2019-11-25 14:28:48 +0800

    CBC

--- HW3/Encrypt/Encrypt.py
+++ HW3/Encrypt/Encrypt.py
@@ -10,68 +10,47 @@ import os
 def padding(data, pad_length):
     return data + bytes([pad_length]*pad_length)
 
+def XOR(blockData,IV):
+    result = bytearray()
+    for blockData, IV in zip(blockData, IV):
+        result.append(blockData ^ IV)
+    return bytes(result)
 
 def encrypt_ECB(data: bytes, key):
     aes = AES.new(key, AES.MODE_ECB)
     blocks=[data[i:i+16]for i in range(0,len(data),16)]
     #print(blocks.shape)
     #print(len(blocks))
-    # cipherBlocks=[]
-    # print(blocks)
-    # cipher=b''
-    # for n in range(0,len(blocks),1):
-    #     cipherBlocks.append(aes.encrypt(blocks[n]))
-    # for n in range(0,len(cipherBlocks),1):
-    #     cipher+=cipherBlocks[n]
-    
-    #cipher=b''
-    #for block in blocks:
-    #    cipher=cipher+aes.encrypt(block)
-    
-    cipher=bytes().join([aes.encrypt(block)for block in blocks])
+    cipherBlocks=[]
+    #print(blocks)
+    cipher=b''
+    for n in range(0,len(blocks),1):
+        cipherBlocks.append(aes.encrypt(blocks[n]))  
+
+    cipher=bytes().join([cipherBlock for cipherBlock in cipherBlocks])  
        
     return cipher
 
 def encrypt_CBC(data: bytes, key):
     aes = AES.new(key, AES.MODE_ECB)
+
+    IV=os.urandom(16)
+    with open(f"IV",'wb') as file:
+        file.write(IV)
+
+    #print("IV:")
+    #print(IV)
     blocks=[data[i:i+16]for i in range(0,len(data),16)]
-    #print(blocks.shape)
-    #print(len(blocks))
     cipherBlocks=[]
-    print(blocks)
     cipher=b''
     for n in range(0,len(blocks),1):
-        cipherBlocks.append(aes.encrypt(blocks[n]))
-    
-    cipher = ''.join(cipherBlocks)
-    
-    #cipher=b''
-    #for block in blocks:
-    #    cipher=cipher+aes.encrypt(block)
-    
-    #cipher=bytes().join([aes.encrypt(block)for block in blocks])
+        tmpBlock = XOR(blocks[n],IV)
+        cipherBlocks.append(aes.encrypt(tmpBlock))
+        IV=cipherBlocks[n]
        
+    cipher=bytes().join([cipherBlock for cipherBlock in cipherBlocks])
     return cipher
 
-# def encrypt_ECB(data, key):
-#     np_img = numpy.array(img)
-#     aes = AES.new(key, AES.MODE_ECB)
-#     inputBlock=""
-#     count=0
-#     outputImg=numpy.zeros((img.height,img.width,3))
-#     tmpCipher=""
-#     for i in range(0,img.height,1):
-#         for j in range(0,img.width,1):
-#             for c in range(0,3,1):
-#                 if(count!=8):
-#                     inputBlock+='{0:08b}'.format(np_img[i][j][c])
-#                     count+=1
-#                     if(count==8):
-#                         tmpCipher=aes.encrypt(inputBlock)
-#                         #count=0
-#                         print(tmpCipher)
-#                         print(b2a_hex(tmpCipher))
-#     return ""
 
 # img = Image.open('./penguin.ppm')
 # width = img.width
@@ -101,7 +80,7 @@ with open(f"key",'wb') as file:
 if(mode=='ECB'):
     cipher = encrypt_ECB(data,random_key)
 if(mode=='CBC'):
-    print("CBC")
+    cipher = encrypt_CBC(data,random_key)
 
 img=Image.open(io.BytesIO(header+cipher[:length]))
 img.save(f'{filename}-{mode}.jpg')
