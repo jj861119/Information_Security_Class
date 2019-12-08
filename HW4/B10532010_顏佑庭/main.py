0d9b5f9cae0f0b16c1b83aa68ed03bca003209ac
jacky861132 <jj861119@gmail.com>
2019-12-08 16:09:45 +0800

    123

--- HW4/B10532010_顏佑庭/main.py
+++ HW4/B10532010_顏佑庭/main.py
@@ -4,50 +4,102 @@ import base64
 import sys
 import io
 import os
+import math
 
-def miller_rabin_test(n):
+def square_and_multiply(H,x,n):
+    H=bin(H)
+    H=H[2:]
+    y=x
+    # print(H)
+    # print(len(H))
+    # print(H[0])
+    for i in range(1,len(H)):
+        y=(y*y)%n
+        if(H[i]=='1'):
+            y=(y*x)%n
+    return y
+
+def witness(a,b):
+    print('test')
+
+def miller_rabin_test(n,ite=20):
+    if(n==2):
+        return True
+    if(not n & 1):
+        return False
     m=n-1
     k=0
     while m % 2==0:
-        m>>=2
+        m//=2
         k+=1
-    a=random.randrange(2,n-2)
-    b=(a**m)%n
-    if(b!=1 and b!=(n-1)):
-        i=1
-        while(i<k and b!=n-1):
-            b=(b**2)%n
-            if(b==1):
-                return False
-            i+=1
-        if(b!=n-1):
+    for i in range(ite):
+        a=random.randrange(2,n-2)
+        b=square_and_multiply(m,a,n)
+        if(b==1 or b==(n-1)):
+            continue
+        for j in range(k-1):
+            b=square_and_multiply(2,b,n)
+            if(b==n-1):
+                break
+        else:
+            return False
+    return True
+
+def check_prime(p):
+    for i in range(0,5):
+        if(not miller_rabin_test(p)):
             return False
     return True
 
 def init_1024():
     p=random.getrandbits(1024)
+    not_prime=True
+    while(not_prime):
+        if(check_prime(p)):
+            not_prime=False
+            break
+        p=random.getrandbits(1024)
 
     q=random.getrandbits(1024)
+    while(not_prime or q==p):
+        if(check_prime(q)):
+            not_prime=False
+            break
+        q=random.getrandbits(1024)
+    return p,q
 
+def GenerateKey(p,q):
+    n=p*q
+    phi_n=(p-1)*(q-1)
+    e=random.randrange(1,phi_n-1)
+    r=math.gcd(e,phi_n)
+    while(r!=1):
+        e=random.randrange(1,phi_n-1)
+        r=math.gcd(e,phi_n)
 
-def init_small():
+#def init_small():
 
 
 action = sys.argv[1]
-random_key = os.urandom(16)
+test = random.getrandbits(1024)
+#print(square_and_multiply(6,7,11))
+# print(test)
+# print(bin(test)[2:])
+#random_key = os.urandom(16)
     
-if(miller_rabin_test(3)):
-    print('true')
-else:
-    print('false')
+# if(check_prime(9973)):
+#     print('true')
+# else:
+#     print('false')
 
 if(action=='init'):
     if(sys.argv[2]=='1024'):
         mode=sys.argv[2]
-        #init_1024()
+        p,q=init_1024()
+
     if(sys.argv[2]=='small'):
         mode=sys.argv[2]
-        init_small()      
+        #init_small()      
 
 
 
